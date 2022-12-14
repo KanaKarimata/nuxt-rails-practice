b15f91b67bfa024faf48d6b15c1ac50a3c9da5b9
KanaKarimata <kana.lt22@gmail.com>
2022-12-14 10:18:44 +0900

    added description that the flow of data

--- front/components/AddTodo.vue
+++ front/components/AddTodo.vue
@@ -9,6 +9,7 @@
           <v-text-filed v-model="title" counter="10" label="todo" required></v-text-filed>
         </v-col>
         <v-col cols="12" md="4">
+          <!-- ＠click＝ボタンをクリックしたときに、methodsの中で定義した関数を呼び出すことができる -->
           <v-btn @click="handleSubmit">作成</v-btn>
         </v-col>
       </v-row>
@@ -20,6 +21,7 @@
   export default {
     data() {
       return {
+        // titleの値は空なのでフォームには初期値として何も入らない
         title: ""
       };
     },
